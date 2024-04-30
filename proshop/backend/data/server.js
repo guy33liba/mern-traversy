c6877c3210326f43264628c784918301bc82428e
guy33liba <guy33liba@gmail.com>
2024-04-30 07:53:59 +0300

    dfasf

---
+++ proshop/backend/data/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import { products } from "./data/products.js"
+import cors from "cors"
+const port = 5000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
+app.get("/api/products", (req, res) => {
+  res.send(products)
+})
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params
+  const product = products.find((p) => p.id === id)
+  res.send(product)
+})
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`)
+})
