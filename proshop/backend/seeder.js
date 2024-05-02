48f69d52e7283ae25c17755331149506d2a6b65a
Guy Liba <guy33liba@gmail.com>
2024-05-02 12:40:28 +0200

    wte

--- proshop/backend/seeder.js
+++ proshop/backend/seeder.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import colors from "colors"
-import users from "/data/users.js"
-import products from "./data/products"
-import User from "./models/UserModel"
-import Product from "./models/ProductModel"
-import Order from "./models/orderModel"
-import { connectDB } from "./config/db"
+import products from "./data/products.js"
+import User from "./models/UserModel.js"
+import Product from "./models/ProductModel.js"
+import Order from "./models/orderModel.js"
+import { connectDB } from "./config/db.js"
+import users from "./data/users.js"
 dotenv.config()
 connectDB()
 
@@ -16,10 +16,34 @@ const importData = async () => {
     await Product.deleteMany()
     await User.deleteMany()
 
-    const createdUsers = await users.insterMany(users)
+    const createdUsers = await User.insertMany(users)
     const adminUser = createdUsers[0]._id
     const sampleProducts = products.map((product) => {
       return { ...product, user: adminUser }
     })
-  } catch (error) {}
+    await Product.insertMany(sampleProducts)
+    console.log("Data Imported".green.inverse)
+    process.exit()
+  } catch (error) {
+    console.log(`${error}`.red.inverse)
+    process.exit
+  }
+}
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+    console.log("Data Destroyed!".red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
+}
+if (process.argv[2] === "-d") {
+  destroyData()
+} else {
+  importData()
 }
