e0c7c2f64f15e42a985b0b941280e513fb48735a
Guy Liba <guy33liba@gmail.com>
2024-05-05 12:55:53 +0200

    redux

--- proshop/frontend/src/screens/HomeScreen.jsx
+++ proshop/frontend/src/screens/HomeScreen.jsx
@@ -2,19 +2,27 @@ import { Row, Col } from "react-bootstrap"
 import Product from "../components/Product"
 import { useGetProductsQuery } from "../slices/productsApiSlice"
 const HomeScreen = () => {
-  const {data:products,isLoading,isError}=useGetProductsQuery()
+  const { data: products, isLoading, error } = useGetProductsQuery()
   return (
     <div>
-      <h1>Lastest Products</h1>
-      <Row>
-        {products.map((product, i) => {
-          return (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product key={i} product={product} />
-            </Col>
-          )
-        })}
-      </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Lastest Products</h1>
+          <Row>
+            {products.map((product, i) => {
+              return (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product key={i} product={product} />
+                </Col>
+              )
+            })}
+          </Row>
+        </>
+      )}
     </div>
   )
 }
