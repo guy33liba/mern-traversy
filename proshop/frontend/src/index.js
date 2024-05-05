8de3a6b1b7e584c850968c13b7a3fa6972541c38
Guy Liba <guy33liba@gmail.com>
2024-05-05 11:25:55 +0200

    redux

--- proshop/frontend/src/index.js
+++ proshop/frontend/src/index.js
@@ -2,21 +2,29 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom"
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
-
+import { Provider } from "react-redux"
+import store from "./store"
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
-    </Route>
-  )
+    </Route>,
+  ),
 )
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
 )
