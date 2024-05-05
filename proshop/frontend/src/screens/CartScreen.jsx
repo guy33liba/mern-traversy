c007eb074f0b01fbb3c3b44f717cc2c7e697096e
guy33liba <guy33liba@gmail.com>
2024-05-05 23:45:24 +0300

    fdsaf

---
+++ proshop/frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap"
+import { FaTrash } from "react-icons/fa"
+import Message from "../components/Message"
+
+const CartScreen = () => {
+  return <div></div>
+}
+
+export default CartScreen
