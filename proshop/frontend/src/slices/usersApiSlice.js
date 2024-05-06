d518578d87242d0d1326a3aae559d7b88e4ea77b
guy33liba <guy33liba@gmail.com>
2024-05-07 02:01:13 +0300

    fasd

--- proshop/frontend/src/slices/usersApiSlice.js
+++ proshop/frontend/src/slices/usersApiSlice.js
@@ -4,10 +4,10 @@ import { apiSlice } from "./apiSlice"
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
-      query: () => ({ url: PRODUCTS_URL }),
+      query: (data) => ({ url: USERS_URL / auth, method: "POST", body: data }),
       keepUnusedDataFor: 5,
     }),
   }),
 })
 
-export const { useGetProductsQuery, useGetProductsDetailsQuery } = productsApiSlice
+export const { useLoginMutation } = usersApiSlice
