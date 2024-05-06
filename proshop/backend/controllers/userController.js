579fa57029d02a4e4b84ee3bbf7a02c91da5b410
guy33liba <guy33liba@gmail.com>
2024-05-07 01:43:17 +0300

    fsad

--- proshop/backend/controllers/userController.js
+++ proshop/backend/controllers/userController.js
@@ -63,7 +63,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 //route GET /api/users/profile
 // access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  const user = User.findOne(req.user._id)
+  const user = await User.findOne(req.user._id)
   if (user) {
     res.status(200).res.send({
       _id: user._id,
@@ -80,7 +80,25 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //route PUT /api/users/profile
 // access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("update User Profile")
+  const user = await User.findOneById(req.user._id)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+    const updateUser = await user.save()
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error("User Not Found")
+  }
 })
 // update  user  profile& clear cookie
 //route GET /api/users/profile
