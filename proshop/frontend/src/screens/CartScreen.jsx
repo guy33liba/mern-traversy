14d56640c628292bcea34fa6cd0926cfa549612a
Guy Liba <guy33liba@gmail.com>
2024-05-06 13:22:19 +0200

    fasd

--- proshop/frontend/src/screens/CartScreen.jsx
+++ proshop/frontend/src/screens/CartScreen.jsx
@@ -31,6 +31,15 @@ const CartScreen = () => {
                     <Link to={`/product/${item._id}`}>{item.name}</Link>
                   </Col>
                   <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control as="select" value={item.qty} onChange={(e) => {}}>
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
                 </Row>
               </ListGroup.Item>
             ))}
