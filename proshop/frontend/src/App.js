f30840dad6b02eb1c5b2e0137a68b478d7777435
guy33liba <guy33liba@gmail.com>
2024-05-07 02:23:44 +0300

    fsda

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap"
 import "./App.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import ToastContainer from "react-toastify"
+import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import { Outlet } from "react-router-dom"
 function App() {
--- proshop/frontend/src/screens/LoginScreen.jsx
+++ proshop/frontend/src/screens/LoginScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import { Form, Button, Row, Col } from "react-bootstrap"
 import FormContainer from "../components/FormContainer"
 const LoginScreen = () => {
