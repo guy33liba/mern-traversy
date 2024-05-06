798459035c92475cbf95bab27189bee8e8ecd043
guy33liba <guy33liba@gmail.com>
2024-05-07 01:08:41 +0300

    logout

--- proshop/backend/controllers/userController.js
+++ proshop/backend/controllers/userController.js
@@ -42,6 +42,8 @@ const registerUser = asyncHandler(async (req, res) => {
 //route POST /api/users/logout
 // access Private
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) })
+  res.status(200).json({ message: "logged out successfully" })
   res.send("logout User ")
 })
 // get  user  profile& clear cookie
--- proshop/backend/routes/userRoutes.js
+++ proshop/backend/routes/userRoutes.js
@@ -11,13 +11,17 @@ import {
   updateUser,
 } from "../controllers/userController.js"
 import express from "express"
-import { protect } from "../middleware/authMiddleWare.js"
+import { protect, admin } from "../middleware/authMiddleWare.js"
 const router = express.Router()
 
-router.route("/").post(registerUser).get(getUsers)
+router.route("/").post(registerUser).get(protect, admin, getUsers)
 router.post("/logout", logoutUser)
-router.post("/authuser", authUser)
+router.post("/auth", authUser)
 router.route("/profile", protect, getUserProfile).put(protect, updateUserProfile)
-router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser)
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(updateUser)
 
 export default router
