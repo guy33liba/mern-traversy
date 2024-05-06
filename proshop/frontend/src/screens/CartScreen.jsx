233c93a0fde6bf9fba030926fe8d6001fa3f629c
Guy Liba <guy33liba@gmail.com>
2024-05-06 14:34:23 +0200

    wsfaf

--- proshop/frontend/src/screens/CartScreen.jsx
+++ proshop/frontend/src/screens/CartScreen.jsx
@@ -39,12 +39,32 @@ const CartScreen = () => {
                         </option>
                       ))}
                     </Form.Control>
+                    <Col md={2}>
+                      <Button type="button" variant="light">
+                        <FaTrash />
+                      </Button>
+                    </Col>
                   </Col>
                 </Row>
               </ListGroup.Item>
             ))}
           </ListGroup>
         )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Subtotal({cartItems.reduce((acc, item) => acc + item.qty, 0)})</h2>${" "}
+              {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button type="button" className="btn-block" disabled={cartItems.length === 0}>
+                Proceed to CheckOut
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Col>
     </Row>
   )
