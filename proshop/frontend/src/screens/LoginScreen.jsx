3524349274464044c526307c8daffe47a24e0d1d
guy33liba <guy33liba@gmail.com>
2024-05-07 02:30:11 +0300

    fasd

--- proshop/frontend/src/screens/LoginScreen.jsx
+++ proshop/frontend/src/screens/LoginScreen.jsx
@@ -7,10 +7,17 @@ import Loader from "../components/Loader"
 import { useLoginMutation } from "../slices/usersApiSlice"
 import { setCredentials } from "../slices/authSlice"
 import { toast } from "react-toastify"
+import { useDispatch, useSelector } from "react-redux"
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [login, { isLoading }] = useLoginMutation()
+  const { userInfo } = useSelector((state) => state.auth)
 
   const submitHandler = (e) => {
     e.preventDefault()
