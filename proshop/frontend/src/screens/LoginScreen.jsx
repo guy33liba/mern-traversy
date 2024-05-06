0dec48db77bf8f927197089019a1dc2fdcaea574
guy33liba <guy33liba@gmail.com>
2024-05-07 02:28:00 +0300

    fasdf

--- proshop/frontend/src/screens/LoginScreen.jsx
+++ proshop/frontend/src/screens/LoginScreen.jsx
@@ -3,6 +3,11 @@ import { useState } from "react"
 import { Link, useLocation, useNavigate } from "react-router-dom"
 import { Form, Button, Row, Col } from "react-bootstrap"
 import FormContainer from "../components/FormContainer"
+import Loader from "../components/Loader"
+import { useLoginMutation } from "../slices/usersApiSlice"
+import { setCredentials } from "../slices/authSlice"
+import { toast } from "react-toastify"
+
 const LoginScreen = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
--- proshop/frontend/src/slices/usersApiSlice.js
+++ proshop/frontend/src/slices/usersApiSlice.js
@@ -4,7 +4,7 @@ import { apiSlice } from "./apiSlice"
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
-      query: (data) => ({ url: USERS_URL / auth, method: "POST", body: data }),
+      query: (data) => ({ url: `${USERS_URL}/auth`, method: "POST", body: data }),
       keepUnusedDataFor: 5,
     }),
   }),
