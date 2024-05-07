2097834f03f2d60d2cc21e8aa8005cd61ccc6db6
Guy Liba <guy33liba@gmail.com>
2024-05-07 12:28:27 +0200

    fdasf

--- proshop/frontend/src/components/Header.jsx
+++ proshop/frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react"
-import { Badge, Navbar, Nav, Container } from "react-bootstrap"
+import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap"
 import { FaShoppingCart, FaUser } from "react-icons/fa"
 import { useSelector } from "react-redux"
 import { LinkContainer } from "react-router-bootstrap"
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart)
+  const { userInfo } = useSelector((state) => state.auth)
+  const logoutHandler = () => {
+    console.log("logout")
+  }
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -31,11 +35,20 @@ const Header = () => {
                   )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser /> Sign in
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link href="/login">
+                    <FaUser /> Sign in
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
