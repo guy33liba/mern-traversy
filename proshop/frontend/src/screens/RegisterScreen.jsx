f4cdabe2f68c4193dd95a5d5b3cbb6328e824170
Guy Liba <guy33liba@gmail.com>
2024-05-07 16:17:24 +0200

    das

--- proshop/frontend/src/screens/RegisterScreen.jsx
+++ proshop/frontend/src/screens/RegisterScreen.jsx
@@ -49,7 +49,15 @@ const RegisterScreen = () => {
     <FormContainer>
       <h1>Sign Up</h1>
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId="email" className="my-3">
+        <Form.Group controlId="name" className="my-3 fs-4">
+          <Form.Label>Name Address</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            value={email}
+            onChange={(e) => setName(e.target.value)}></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="email" className="my-3 fs-4">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type="email"
@@ -57,16 +65,25 @@ const RegisterScreen = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}></Form.Control>
         </Form.Group>
-        <Form.Group controlId="password" className="my-3">
-          <Form.Label>Password </Form.Label>
+
+        <Form.Group controlId="password" className="fs-4">
+          <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Enter password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}></Form.Control>
         </Form.Group>
+        <Form.Group controlId="confirmPassword" className="my-3 fs-4">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+        </Form.Group>
         <Button type="submit" variant="primary" className="mt-2" disabled={isLoading}>
-          Sign In
+          Sign Up
         </Button>
         {isLoading && <Loader />}
       </Form>
