8ec3aa984ee950e2ed9a44497f23655fdc798f1e
guy33liba <guy33liba@gmail.com>
2024-05-07 10:20:20 +0300

    dasfsd

--- proshop/backend/middleware/authMiddleWare.js
+++ proshop/backend/middleware/authMiddleWare.js
@@ -5,38 +5,25 @@ import User from "../models/UserModel.js"
 //Protect Routes
 const protect = asyncHandler(async (req, res, next) => {
   let token
+  //read the jwt from the cooke
 
-  // Read the JWT from the cookie
   token = req.cookies.jwt
-
-  if (!token) {
-    // If token is missing, respond with 401 Unauthorized
-    res.status(401)
-    throw new Error("Not authorized, no token")
-  }
-
-  try {
-    // Verify the JWT token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-    // Find the user associated with the decoded token
-    req.user = await User.findById(decoded.UserId).select("-password")
-
-    if (!req.user) {
-      // If user doesn't exist, respond with 401 Unauthorized
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      req.user = await User.findById(decoded.UserId).select("-password")
+      next()
+    } catch (error) {
+      console.log(error)
       res.status(401)
-      throw new Error("Not authorized, user not found")
+      throw new Error("not authorized token failed")
     }
-
-    // If everything is valid, proceed to the next middleware
-    next()
-  } catch (error) {
-    // Handle any errors during token verification
-    console.error(error)
+  } else {
     res.status(401)
-    throw new Error("Not authorized, token verification failed")
+    throw new Error("not authorized, no token")
   }
 })
+
 //Admin MiddleWare
 
 const admin = (req, res, next) => {
