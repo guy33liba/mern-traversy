a9714c88d938c2b95a67f35820ae72e03a6ca53f
Guy Liba <guy33liba@gmail.com>
2024-05-08 13:01:28 +0200

    fdasf

--- proshop/frontend/src/screens/ShippingScreen.jsx
+++ proshop/frontend/src/screens/ShippingScreen.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react"
-    FormContainer
+import { Form, Button } from "react-bootstrap"
+import FormContainer from "../components/FormContainer"
 const ShippingScreen = () => {
+  const [address, setAddress] = useState("")
+  const [city, setCity] = useState("")
+  const [postalCode, setPostalCode] = useState("")
+  const [country, setCountry] = useState("")
   return <div></div>
 }
 
