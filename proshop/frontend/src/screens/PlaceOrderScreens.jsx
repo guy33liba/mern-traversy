437c6fa2ba13ac25c87f7a13adc4e9a6bf3d5125
Guy Liba <guy33liba@gmail.com>
2024-05-09 12:25:15 +0200

    fasd

--- proshop/frontend/src/screens/PlaceOrderScreens.jsx
+++ proshop/frontend/src/screens/PlaceOrderScreens.jsx
@@ -18,27 +18,51 @@ const PlaceOrderScreens = () => {
     } else if (!cart.paymentMethod) {
       navigate("/payment")
     }
-  }, [cart.paymentMethod, cart.shippingAddress, navigate])
+  }, [cart.paymentMethod, cart.shippingAddress.address, navigate])
 
   return (
-    <CheckoutSteps step1 step2 step3 step4>
-      <Row>
-        <Col md={8}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h2>Shipping</h2>
-              <p>
-                <strong>Address:</strong>
-                {cart.shippingAddress.address},{cart.shippingAddress.city}
-                {cart.shippingAddress.postalCode}
-                {cart.shippingAddress.country}
-              </p>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={4}>Column</Col>
-      </Row>
-    </CheckoutSteps>
+    <>
+      <CheckoutSteps step1 step2 step3 step4>
+        <Row>
+          <Col md={8}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Shipping</h2>
+                <p>
+                  <strong>Address:</strong>
+                  {cart.shippingAddress.address},{cart.shippingAddress.city}
+                  {cart.shippingAddress.postalCode}
+                  {cart.shippingAddress.country}
+                </p>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <h2>Shipping</h2>
+                <p>
+                  <strong>Payment Method:</strong>
+                  {cart.paymentMethod}
+                </p>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <h2>Order Items</h2>
+                <p>
+                  <strong>Payment Method:</strong>
+                  {cart.cartItems.length === 0 ? (
+                    <Message>Your Cart is Empty</Message>
+                  ) : (
+                    <ListGroup variant="flush">
+                      {cart.orderItems.map((item, index) => (
+                        <ListGroup.Item key={index}> </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                  )}
+                </p>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={4}>Column</Col>
+        </Row>
+      </CheckoutSteps>
+    </>
   )
 }
 
